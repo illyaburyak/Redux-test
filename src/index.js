264682c2c94d52d396d35d77e5bbdd0d031a7c58
Illya Buryak <illyabenavente@gmail.com>
2020-11-28 18:36:26 -0600

    Playing with redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+    switch (action.type){
+        case 'RND':
+            return state + action.payload;
+
+        case 'INC':
+            return state + 1;
+
+        case 'DEC':
+            return state - 1;
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+document.getElementById('inc')
+    .addEventListener('click', () => {
+       store.dispatch({type: 'INC'})
+    });
+
+
+document.getElementById('dec')
+    .addEventListener('click', () => {
+        store.dispatch({type: 'DEC'})
+    });
+
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random() * 10);
+        store.dispatch({
+            type: 'RND',
+            payload
+        })
+    });
+
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState();
+};
+
+store.subscribe(update);
